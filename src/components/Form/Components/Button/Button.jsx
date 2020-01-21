836f8a063d8b437499603c7a317ee47a46bad80c
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-01-21 12:38:57 +0300

    feat: added validation btnNewToDo

--- src/components/Form/Components/Button/Button.jsx
+++ src/components/Form/Components/Button/Button.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import styles from './Button.module.css';
 
 function Button(props) {
+    if(props.todoName.length > 3){
 
-    const addToDo = () => {
-        const id = Date.now();
-        const todo = {
-            name: props.todoName,
-            descr: props.todoDescr,
-            id
-        }
-        props.addNewTodo(todo);
-        props.clearInputName();
-        props.clearInputDescr();
-    };
+        const addToDo = () => {
+            const id = Date.now();
 
+            const todo = {
+                name: props.todoName,
+                descr: props.todoDescr,
+                id
+            };
 
-    return (
-        <button onClick={addToDo} className={styles.btnNewTodo}>Add ToDo</button>
-    )
+            props.addNewTodo(todo);
+            props.clearInputName();
+            props.clearInputDescr();
+        };
+
+        return (
+            <button onClick={addToDo} className={styles.btnNewTodo}>Add ToDo</button>
+        )
+    } else {
+        return <button disabled={true} className={styles.btnNewTodoDisabled}>Add ToDo</button>
+    }
 }
 
 export default Button;
