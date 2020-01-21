57f62b691b4ac3b7877ac6eeddfe494ad5c28eb3
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-01-21 13:10:37 +0300

    feat: added message 'no task descripion' to modalWindow

--- src/components/ModalWindow/components/modalWindowContent/ModalWindowContent.jsx
+++ src/components/ModalWindow/components/modalWindowContent/ModalWindowContent.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import styles from './modalWindowContent.module.css';
 
 function modalWindowContent(props) {
-    return (
-        <div className={styles.modalWindowContent}>
-            <p>{props.text}</p>
-        </div>
-    )
+    console.log('text', props.text.length);
+    if(props.text.length > 0){
+        return (
+            <div className={styles.modalWindowContent}>
+                <p>{props.text}</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.modalWindowContent}>
+                <p className={styles.messageNoTaskDescription}>No task description!</p>
+            </div>
+        )
+    }
 }
 
 export default modalWindowContent;
